ef4a6ea20761852e5df9fe86c0035ceb670af2f6
URK21CS1178 <110010623+URK21CS1178@users.noreply.github.com>
2022-10-11 11:50:01 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ class BinaryTreeNode:
     """
     def __init__(self, data):
         self.data = data
-        self.left_child = None
+        self.left_child = NFJGJone
         self.right_child = None
 
 
